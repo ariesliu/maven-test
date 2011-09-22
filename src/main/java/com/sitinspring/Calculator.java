286be99c7af904c50f5cffe3eec66ecd49119a4d
junliu999 <junliu@cn.ufinity.com>
2011-09-22 11:33:18 +0800

    test

---
+++ src/main/java/com/sitinspring/Calculator.java
@@ -0,0 +1,15 @@
+package com.sitinspring;
+
+public class Calculator {
+    public static void main(String[] arg) {
+        System.out.println("Hello!Maven!");
+    }
+
+    public int add(int op1, int op2){
+        return op1 + op2;
+    }
+
+    public int sub(int op1, int op2){
+        return op1 - op2;
+    }
+}
